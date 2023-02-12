e551f17bc8e913fd38fcf98263f1d362c1b5c480
Beatriz Lemos <10408674+lemosbc@users.noreply.github.com>
2023-02-12 23:12:41 +0000

    Add functionality to change grid size.

--- scripts.js
+++ scripts.js
@@ -16,3 +16,37 @@ for (let i = 0; i < cellList.length; i++) {
     cellList[i].style.backgroundColor = "pink";
   });
 }
+
+function removeElementsByClass(className) {
+  const elements = document.getElementsByClassName(className);
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+}
+
+function gridLayout(gridSide) {
+  document.getElementById(
+    "container"
+  ).style.gridTemplateColumns = `repeat(${gridSide}, 1fr)`;
+  document.getElementById(
+    "container"
+  ).style.gridTemplateRows = `repeat(${gridSide}, 1fr)`;
+}
+
+let side;
+
+function promptMe() {
+  do {
+    side = prompt("Enter number of squares per side (max 100)");
+  } while (side > 100);
+
+  sideSquared = side * side;
+
+  removeElementsByClass("cell");
+
+  makeDivs(sideSquared);
+
+  gridLayout(side);
+}
+
+document.getElementById("grid-button").addEventListener("click", promptMe);
