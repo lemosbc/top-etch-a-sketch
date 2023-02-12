e7b2c6467ce79ef5e7357d6f7c2ec1f4d4fcc419
Beatriz Lemos <10408674+lemosbc@users.noreply.github.com>
2023-02-12 00:55:15 +0000

    Hover effect working with javascript.

--- scripts.js
+++ scripts.js
@@ -3,8 +3,16 @@ var cell;
 function makeDivs(numDivs) {
   for (let i = 0; i < numDivs; i++) {
     cell = document.createElement("div");
+    cell.classList.add("cell");
     document.getElementById("container").appendChild(cell);
   }
 }
 
 makeDivs(256);
+
+let cellList = document.getElementsByClassName("cell");
+for (let i = 0; i < cellList.length; i++) {
+  cellList[i].addEventListener("mouseover", function () {
+    cellList[i].style.backgroundColor = "pink";
+  });
+}
